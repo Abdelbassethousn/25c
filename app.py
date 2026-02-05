038f03d3d2d851380c27ab590f61cc3b5807e48b
Abdelbassethousn <housniabdelbassetus@gmail.com>
2026-02-05 16:28:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ print("Hello, World")
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print (f"iteration {i} is {name}")
+
+#new comment from the console
 #new comments to psuh
